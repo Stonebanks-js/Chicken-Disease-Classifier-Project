2bc9c4708f81797d630a503bed4b5cfad601b331
Stonebanks-js <aradhyachdry@outlook.com>
2024-10-23 23:16:41 +0530

    Deployement App Ready

---
+++ src/cnnClassifier/pipeline/predict.py
@@ -0,0 +1,27 @@
+import numpy as np
+import os
+from tensorflow.keras.models import load_model # type: ignore
+from  tensorflow.keras.preprocessing import image # type: ignore
+
+
+class PredictionPipeline:
+    def __init__(self,filename):
+        self.filename = filename
+
+    def predict(self):
+        # load model
+        model = load_model(os.path.join("artifacts","training", "model.keras"))
+
+        imagename = self.filename
+        test_image = image.load_img(imagename, target_size = (224,224))
+        test_image = image.img_to_array(test_image)
+        test_image = np.expand_dims(test_image, axis = 0)
+        result = np.argmax(model.predict(test_image), axis=1)
+        print(result)
+
+        if result[0] == 1:
+            prediction = 'Healthy'
+            return [{ "image" : prediction}]
+        else:
+            prediction = 'Coccidiosis'
+            return [{ "image" : prediction}]
